a0c5e2fb6ed9b3e4cb7f6ef8d649b86aca767117
Mert <mertcnat@gmail.com>
2019-07-31 10:38:28 +0100

    Testing and debugging to ensure code meets specififcations
    Signed-off-by: Mert <mertcnat@gmail.com>

--- projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/SareetaApplication.java
+++ projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/SareetaApplication.java
@@ -1,12 +1,15 @@
 package com.example.demo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@Configuration
 @EnableJpaRepositories("com.example.demo.model.persistence.repositories")
 @EntityScan("com.example.demo.model.persistence")
 @SpringBootApplication
@@ -19,6 +22,12 @@ public class SareetaApplication {
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		return modelMapper;
 	}
 
 	public static void main(String[] args) {
